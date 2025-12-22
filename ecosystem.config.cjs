facb7c7e75d8613af24deb5969758c45e86bf59a
m6rc0sp <paulo.marcos0108@live.com>
2025-12-22 14:51:06 -0300

    restaura configuração do script e argumentos no ecosystem para uso do serve

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,8 +2,8 @@ module.exports = {
     apps: [{
         name: "gerenciador-oticas-front",
         cwd: "/home/m6rc0sp/Documentos/MVL/Nexos App/gerenciador-de-oticas-front",
-        script: "node_modules/.bin/vite",
-        args: "preview --port 10003 --host 0.0.0.0",
+        script: "serve",
+        args: "-s dist -l 10003",
         env_production: {
             NODE_ENV: "production"
         }
