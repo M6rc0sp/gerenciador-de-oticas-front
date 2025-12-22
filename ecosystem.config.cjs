01b86998c96faa0a3acfff01798e2e1ce5ba54eb
m6rc0sp <paulo.marcos0108@live.com>
2025-12-22 14:59:19 -0300

    ajusta o caminho do diretório de trabalho no arquivo ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
         name: "gerenciador-oticas-front",
-        cwd: "/home/m6rc0sp/Documentos/MVL/Nexos App/gerenciador-de-oticas-front",
+        cwd: "/home/documents/mvl/gerenciador-de-oticas-front",
         script: "serve",
         args: "-s dist -l 10003",
         env_production: {
