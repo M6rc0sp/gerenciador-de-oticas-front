ceab12fea04d82184485fac6f61c0896d8551b51
m6rc0sp <paulo.marcos0108@live.com>
2025-12-22 14:12:50 -0300

    ajuste a formatação do arquivo ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,11 +1,11 @@
 module.exports = {
-  apps : [{
-    name   : "gerenciador-oticas-front",
-    cwd    : "/home/m6rc0sp/Documentos/MVL/Nexos App/gerenciador-de-oticas-front", // Certifique-se de que este caminho está correto no seu servidor
-    script : "npm",
-    args   : "run serve",
-    env_production: {
-      NODE_ENV: "production"
-    }
-  }]
+    apps: [{
+        name: "gerenciador-oticas-front",
+        cwd: "/home/m6rc0sp/Documentos/MVL/Nexos App/gerenciador-de-oticas-front", // Certifique-se de que este caminho está correto no seu servidor
+        script: "npm",
+        args: "run serve",
+        env_production: {
+            NODE_ENV: "production"
+        }
+    }]
 };
