4ac0da47c82ce24d9b19f815642572539715770d
m6rc0sp <paulo.marcos0108@live.com>
2025-12-22 15:04:30 -0300

    ajusta a formatação dos argumentos no arquivo ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
         name: "gerenciador-oticas-front",
         cwd: "/home/documents/mvl/gerenciador-de-oticas-front",
         script: "serve",
-        args: "-s dist -l 10003",
+        args: ["-s", "dist", "-l", "10003"],
         env_production: {
             NODE_ENV: "production"
         }
