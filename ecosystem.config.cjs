e09962341b889a38a851dadfe7111c4735ce0fc4
m6rc0sp <paulo.marcos0108@live.com>
2025-12-22 14:12:39 -0300

    remove setup scripts and add ecosystem configuration for production

---
+++ ecosystem.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  apps : [{
+    name   : "gerenciador-oticas-front",
+    cwd    : "/home/m6rc0sp/Documentos/MVL/Nexos App/gerenciador-de-oticas-front", // Certifique-se de que este caminho está correto no seu servidor
+    script : "npm",
+    args   : "run serve",
+    env_production: {
+      NODE_ENV: "production"
+    }
+  }]
+};
