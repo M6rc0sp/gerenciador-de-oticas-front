48f0551535d258e3eca36e93d96ae655fbbc0074
m6rc0sp <paulo.marcos0108@live.com>
2025-12-22 15:11:53 -0300

    corrige o caminho do script no arquivo ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
     apps: [{
         name: "gerenciador-oticas-front",
         cwd: "/home/documents/mvl/gerenciador-de-oticas-front",
-        script: "serve",
+        script: "/usr/local/bin/serve",
         args: ["-s", "dist", "-l", "10003"],
         env_production: {
             NODE_ENV: "production"
